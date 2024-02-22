a65307a343af6d927248b7e94cb978db032ddbca
SAMYSBL <samara.barcelos@live.com>
2024-02-22 15:12:13 -0300

    components dinamics property

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input,
+   OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -7,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BigCardComponent implements OnInit {
 
+  @Input()
+PhotoCover:string =""
+@Input()
+cardTitle:string=""
+@Input()
+cardDescription:string=""
   constructor() { }
 
   ngOnInit(): void {
